696cffb320f4402638430705712881a0112aeed7
coder-boner <pink.agent47@gmail.com>
2024-08-02 19:33:36 +0800

    Add files via upload

---
+++ ClientSide/ordering.py
@@ -0,0 +1,7 @@
+orderinput = input('Would you like to order. You HAVE to use the item id: ')
+
+if orderinput == '9' :
+    print("You have ordered HILO MILK 1L") 
+    
+elif orderinput == '10' :
+    print("You have ordered SKIM MILK 1L") 
